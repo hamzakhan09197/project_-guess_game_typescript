fc2e64feb5dc8e940b4943da71f3e8e2f9c895ee
hamzakhan09197 <154448652+hamzakhan09197@users.noreply.github.com>
2024-01-19 16:51:08 +0500

    adding files

---
+++ guess_game.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import showBanner from "node-banner";
+import { createSpinner } from 'nanospinner';
+let sleep = () => new Promise((r) => setTimeout(r, 2000));
+async function banner() {
+    await showBanner('Number Guessing Game', 'this program genrate a  random number that you have to guess.');
+}
+let startGame = async () => {
+    let { deficulty } = await inquirer.prompt({
+        name: "deficulty",
+        type: "list",
+        choices: ["Easy", "Medium", "Deficult"],
+        message: chalk.blueBright("Please Select Deficulty  Level"),
+    });
+    let { rounds } = await inquirer.prompt({
+        name: "rounds",
+        type: "number",
+        message: chalk.rgb(245, 162, 162)("How many rounds you want to play (Max 5)"),
+        validate: (input) => {
+            if (isNaN(input) || input < 1 || input > 5) {
+                return "Enter number around 1 to 5";
+            }
+            else
+                return true;
+        }
+    });
+    let correctAns = 0;
+    let guessCount = 0;
+    let number_rang = deficulty === "Easy" ? 5 : deficulty === "Medium" ? 10 : 15;
+    while (guessCount < rounds) {
+        let rendom_number = Math.floor(Math.random() * number_rang);
+        let { userInput } = await inquirer.prompt({
+            name: "userInput",
+            type: "number",
+            message: chalk.rgb(245, 162, 162)(`please Enter number between 1 to ${number_rang}`),
+            validate: (input) => {
+                if (isNaN(input) || input > number_rang)
+                    return `Please Enter between t to ${number_rang} `;
+                else
+                    return true;
+            }
+        });
+        guessCount++;
+        const spinner = createSpinner('Guessing').start();
+        await sleep();
+        if (rendom_number === userInput) {
+            spinner.success({ text: chalk.greenBright("Congratulaions You win") });
+            correctAns++;
+        }
+        else {
+            spinner.error({ text: chalk.redBright("Ooops! You lose") });
+        }
+        ;
+    }
+    const spinner = createSpinner('Resulting').start();
+    await sleep();
+    spinner.success({ text: chalk.yellowBright(`You have guessed ${correctAns} out of ${guessCount}`) });
+    let { playAgain } = await inquirer.prompt({
+        name: "playAgain",
+        type: "confirm",
+        message: chalk.rgb(245, 162, 162)('If you want to paly again then press "Y" otherwise "N". ')
+    });
+    if (playAgain) {
+        startGame();
+    }
+    ;
+};
+await banner();
+startGame();
